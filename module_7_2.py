e94200c7a2204a98eda95f80809dcb5540add2ad
Сергеич <Dumat.msk@hotmail.com>
2024-12-17 22:03:02 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,17 +1,21 @@
 from pprint import pprint
-def custom_write(file_name, strings):
-    file_name = 'text.txt'
-    file = open(file_name, 'w', encoding = 'utf-8')
-    strings_positions = {}
-    str_num = 0
-    for string in strings:
-        str_num += 1
-        start_byte = file.tell()
-        file.write(f'{string}\n')
-        strings_positions[(str_num, start_byte)] = string
-    file.close()
-    return strings_positions
 
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    # определяем начальные значения
+    string_positions = {}   # задаём словарь
+    str_num = 0
+    str_start_byte = file.seek(0)   # байт начала первой строки
+    for string_ in strings:
+        file.write(string_+'\n')
+        str_num += 1
+        key = (str_num, str_start_byte) # задаём ключи словаря
+        string_positions[key]= string_  # добавляем значения в словарь
+        str_start_byte = file.tell()
+    file.close()
+    return string_positions
+
+file_name = 'newfile.txt'   # задаём имя нашего файла
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
@@ -22,3 +26,4 @@ info = [
 result = custom_write('test.txt', info)
 for elem in result.items():
   print(elem)
+
